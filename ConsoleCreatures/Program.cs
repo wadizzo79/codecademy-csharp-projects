03e6725f34e75ef663c386cabd046308e7fe17fd
wadizzo79 <caywadawi7@gmail.com>
2025-05-26 20:10:08 +0300

    Initial commit

---
+++ ConsoleCreatures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            //The purpose of this code is to build a creature
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| o |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("Sasquach");
+        }
+    }
+}

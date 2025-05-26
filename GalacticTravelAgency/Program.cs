b622b6f3bc8425c33e83fefb030342131fa4ef4f
wadizzo79 <caywadawi7@gmail.com>
2025-05-26 20:15:23 +0300

    Initial commit of multiple C# projects

---
+++ GalacticTravelAgency/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class GalacticTravelAgency
+{
+    public static void Main(string[] args)
+    {
+        // This program simulates a galactic travel agency booking system.
+        string passengerName = "Zara";
+        int passengerAge = 28;
+        string ticketType = "First Class";
+        string preferredPlanet = "Mars";
+
+        // Simulating a scenario where the passenger's age is incremented until they are older than 28
+        while (passengerAge <= 28) // Loop to simulate time passing until the passenger is older than 28
+        {
+            passengerAge++; // Incrementing age to simulate time passing
+        }
+
+        double passengerAgeDouble = (double)passengerAge; // Explicitly casting int to double
+        double passengerAgeDouble2 = passengerAge; // Implicitly casting int to double
+
+        string passengerAgeString = Convert.ToString(passengerAge); // Converting int to string
+
+        // Output the passenger details
+        Console.WriteLine(passengerName);
+        Console.WriteLine(passengerAgeString);
+        Console.WriteLine(ticketType);
+        Console.WriteLine(preferredPlanet);
+    }
+}
